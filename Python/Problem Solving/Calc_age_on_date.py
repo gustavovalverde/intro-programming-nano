36a3cd422f26cf8ba7d11bcb2ab99186004041d6
Valverde <gvalverde@deloitte.com>
2016-02-03 20:32:23 -0400

    New problem: age calculator based on date

---
+++ Python/Problem Solving/Calc_age_on_date.py
@@ -0,0 +1,20 @@
+# Given your birthday and the current date, calculate your age
+# in days. Compensate for leap days. Assume that the birthday
+# and current date are correct dates (and no time travel).
+# Simply put, if you were born 1 Jan 2012 and todays date is
+# 2 Jan 2012 you are 1 day old.
+
+daysOfMonths = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def isLeapYear(year):
+    ##
+    # Your code here. Return True or False
+    # Pseudo code for this algorithm is found at
+    # http://en.wikipedia.org/wiki/Leap_year#Algorithm
+    ##
+
+def daysBetweenDates(y1, m1, d1, y2, m2, d2):
+    ##
+    # Your code here.
+    ##
+    return days
