d9d7d0defd4a9e1112e65bc9ee104be939717627
Gustavo A. Valverde <Gustavo A. Valverde>
2016-02-08 12:10:28 -0400

    Add test cases and change daysBetweenDates so it takes into account when calculating into a Leap Year and after Frebuary.

--- Python/Problem Solving/Calc_age_on_date.py
+++ Python/Problem Solving/Calc_age_on_date.py
@@ -35,18 +35,47 @@ def isDateValid(y1, m1, d1, y2, m2, d2):
         return False
 
 
-def daysBetweenDates(y1, m1, d1, y2, m2, d2):
+def daysinYear(y1, y2):
     days = 0
-    BirthDate = sum(daysOfMonths[0: m1 - 1]) + d1
-    CurrentDate = sum(daysOfMonths[0: m2 - 1]) + d2
-    if isDateValid(y1, m1, d1, y2, m2, d2) is True:
-        if y1 < y2:
-            for y in range(y1, y2):
-                if isLeapYear(y) is True:
-                    days += 366
-                else:
-                    days += 365
-        days = days + CurrentDate - BirthDate
-        print "You are " + str(days) + " old"
+    if y1 < y2:
+        for y in range(y1, y2):
+            if isLeapYear(y) is True:
+                days += 366
+            else:
+                days += 365
+    return days
 
-daysBetweenDates(2001, 01, 20, 2002, 04, 26)
+
+def daysInMonth(m1, d1, m2, d2):
+    birthDate = sum(daysOfMonths[0: m1 - 1]) + d1
+    currentDate = sum(daysOfMonths[0: m2 - 1]) + d2
+    currentDays = currentDate - birthDate
+    return currentDays
+
+
+def daysBetweenDates(y1, m1, d1, y2, m2, d2):
+    finalDays = daysinYear(y1, y2) + daysInMonth(m1, d1, m2, d2)
+    if ((isLeapYear(y1) is True) and (m1 >= 3)) or (
+        (isLeapYear(y2) is True) and (m2 >= 3)):
+        finalDays += 1
+
+    print "You are " + str(finalDays) + " days old"
+    return finalDays
+
+
+def test():
+    test_cases = [((2012, 1, 1, 2012, 2, 28), 58),
+                  ((2012, 1, 1, 2012, 3, 1), 60),
+                  ((2011, 6, 30, 2012, 6, 30), 366),
+                  ((2011, 1, 1, 2012, 8, 8), 585),
+                  ((1900, 1, 1, 1999, 12, 31), 36523)]
+
+    for (args, answer) in test_cases:
+        result = daysBetweenDates(*args)
+        if result != answer:
+            print "Test with data:", args, "failed"
+            print result
+        else:
+            print "Test case passed!"
+
+test()
