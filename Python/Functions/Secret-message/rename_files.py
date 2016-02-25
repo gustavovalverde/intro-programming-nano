c730ee72c8e82b452afbd70c9e94d9da1c6bf74d
Gustavo A. Valverde <Gustavo A. Valverde>
2016-02-25 07:35:13 -0400

    All changes made, program working fully

--- Python/Functions/Secret-message/rename_files.py
+++ Python/Functions/Secret-message/rename_files.py
@@ -2,6 +2,13 @@ import os
 
 
 def rename_files():
-    os.listdir("C:\Users\GustavoA\intro-programming-nano\Python\Functions\Secret message\prank")
+    files_path = os.listdir("C:\Users\GustavoA\intro-programming-nano"
+                            "\Python\Functions\Secret-message\prank")
+    saved_path = os.getcwd()
+    os.chdir("C:\Users\GustavoA\intro-programming-nano\Python\Functions"
+             "\Secret-message\prank")
+    for file_name in files_path:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+    os.chdir(saved_path)
 
-print rename_files()
+rename_files()
