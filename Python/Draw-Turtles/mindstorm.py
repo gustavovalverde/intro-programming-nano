98100b02f0139f53334d76eab181ce1d6daa08f2
Gustavo A. Valverde <Gustavo A. Valverde>
2016-02-25 22:20:59 -0400

    Initial Turtle commit

---
+++ Python/Draw-Turtles/mindstorm.py
@@ -0,0 +1,13 @@
+import turtle
+
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.forward(100)
+
+    window.exitonclick()
+
+draw_square()
