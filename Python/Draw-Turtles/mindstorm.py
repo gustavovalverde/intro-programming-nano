6ce0b859add941fd6cb594ad3e17eccb9c6699c1
vagrant <g.valverde02@gmail.com>
2016-03-01 21:35:35 -0400

    Add some Turtle magic. Time to make changes.

--- Python/Draw-Turtles/mindstorm.py
+++ Python/Draw-Turtles/mindstorm.py
@@ -3,10 +3,31 @@ import turtle
 
 def draw_square():
     window = turtle.Screen()
-    window.bgcolor("red")
+    window.bgcolor("grey")
 
-    brad = turtle.Turtle()
-    brad.forward(100)
+    gus = turtle.Turtle()
+
+    # customizing gus
+    gus.shape("turtle")
+    gus.color("blue")
+    gus.speed(1)
+
+    gus.forward(100)
+    gus.right(90)
+    gus.forward(100)
+    gus.right(90)
+    gus.forward(100)
+    gus.right(90)
+    gus.forward(100)
+    gus.right(90)
+
+    # adding a new friend
+    ros = turtle.Turtle()
+    ros.shape("classic")
+    ros.color("pink")
+    ros.speed(3)
+
+    ros.circle(100)
 
     window.exitonclick()
 
