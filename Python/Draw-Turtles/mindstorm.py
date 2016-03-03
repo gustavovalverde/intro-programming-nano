733c18177393926f299f127d127dd3e6cc2aea41
vagrant <g.valverde02@gmail.com>
2016-03-03 08:01:59 -0400

    Add triangle figure and changed canvas logic

--- Python/Draw-Turtles/mindstorm.py
+++ Python/Draw-Turtles/mindstorm.py
@@ -1,37 +1,47 @@
 import turtle
 
 
-def canvas():
+def draws():
     window = turtle.Screen()
     window.bgcolor("grey")
+    draw_square()
+    draw_circle()
+    draw_triangle()
+    window.exitonclick()
 
 
 def draw_square():
-    gus = turtle.Turtle()
+    box = turtle.Turtle()
+    box.shape("turtle")
+    box.color("blue")
+    box.speed(1)
 
-    # customizing gus
-    gus.shape("turtle")
-    gus.color("blue")
-    gus.speed(1)
-
-    pos = 0
-    while pos < 4:
-        gus.forward(100)
-        gus.right(90)
-        pos += 1
+    sides = 0
+    while sides < 4:
+        box.forward(100)
+        box.right(90)
+        sides += 1
 
 
 def draw_circle():
-    # adding a new friend
-    ros = turtle.Turtle()
-    ros.shape("classic")
-    ros.color("pink")
-    ros.speed(3)
+    donna = turtle.Turtle()
+    donna.shape("classic")
+    donna.color("pink")
+    donna.speed(3)
 
-    ros.circle(100)
+    donna.circle(100)
 
 
-canvas()
-draw_square()
-draw_circle()
-turtle.Screen().exitonclick()
+def draw_triangle():
+    egypt = turtle.Turtle()
+    egypt.shape("turtle")
+    egypt.color("orange")
+    egypt.speed(1)
+
+    sides = 0
+    while sides < 3:
+        egypt.forward(-100)
+        egypt.left(120)
+        sides += 1
+
+draws()
