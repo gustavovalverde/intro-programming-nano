aa4eb86efaff85eabde74b9a0f18b6515c0620cf
vagrant <g.valverde02@gmail.com>
2016-03-04 19:51:37 -0400

    Job done. Code can get better, will be working on that later.

--- Python/Draw-Turtles/mini-project.py
+++ Python/Draw-Turtles/mini-project.py
@@ -9,8 +9,14 @@ def draw_flower():
     pikachu = pencil(2, "arrow", "yellow", 2)
     hulk = pencil(2, "arrow", "green", 2)
 
+    num_petals = 8
+    for petal in xrange(num_petals):
+        rose.setheading(0)
+        rose.right(360 * petal/num_petals)
+        draw_petal(rose, 75, 8)
+
     draw_center(pikachu)
-    draw_petal(rose)
+    draw_stem(hulk, 50, 8)
 
     window.exitonclick()
 
@@ -24,25 +30,25 @@ def pencil(penSize, penShape, penColor, penSpeed):
     myPen.speed(penSpeed)
     return myPen
 
-num_petals = 8
 
-def draw_petal(myPen, num_petals, radius, steps):
+def draw_petal(myPen, radius, steps):
     myPen.circle(radius, 90, steps)
     myPen.left(90)
     myPen.circle(radius, 90, steps)
-    steps = 8
-radius = 100
-
-for petal in xrange(num_petals):
-    myPen.setheading(0)
-    myPen.right(360 * petal/num_petals)
-    draw_petal(radius, steps)
-
-
-
 
 
 def draw_center(myPen):
     myPen.dot(30)
+
+
+def draw_stem(myPen, radius, steps):
+    myPen.right(90)
+    myPen.forward(175)
+    myPen.setheading(0)
+    draw_petal(myPen, radius, steps)
+    myPen.setheading(90)
+    draw_petal(myPen, radius, steps)
+    myPen.right(90)
+    myPen.forward(75)
 
 draw_flower()
