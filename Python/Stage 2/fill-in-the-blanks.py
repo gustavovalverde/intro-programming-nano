ce6f5a40af26b18cb07300f508ac051c05c5cf22
Gustavo Valverde <g.valverde02@gmail.com>
2016-03-26 23:59:59 +0000

    Half working. play_game() logic not tested yet. First steps are printing as supposed.

--- Python/Stage 2/fill-in-the-blanks.py
+++ Python/Stage 2/fill-in-the-blanks.py
@@ -3,7 +3,7 @@
 # This can be used as a study tool to help you remember important vocabulary!
 blanks = ["___1___", "___2___", "___3___", "___4___", "___5___"]
 
-easy_answers = ["Function", "arguments", "None", "dictionaries"]
+easy_answers = ["function", "arguments", "None", "set"]
 
 easy_statement = '''A ___1___ is created with the def keyword. You specify the
 inputs a ___1___ takes by adding ___2___ separated by commas between the
@@ -11,9 +11,6 @@ parentheses. ___1___s by default return ___3___ if you don't specify the value
 to return. ___2___ can be standard data types such as string, number,
 dictionary, tuple, and ___4___ or can be more complicated such as objects and
 lambda functions.'''
-
-# If you need help, you can sign up for a 1 on 1 coaching appointment:
-# https://calendly.com/ipnd1-1/20min/
 
 
 def Difficulty():
@@ -49,36 +46,37 @@ def Difficulty():
         hard_mode(guess_input)
     else:
         print "That's not an option!"
-        continue
 
 # def easy_mode(guess_input):
 #     guesses = 0
 #    while guesses < guess_input:
 
 
-def answer_in_statement(user_answer, answers):
-    for answer in answers:
-        if answer in user_answer:
+def answer_in_statement(blank, blanks):
+    for answer in blanks:
+        if answer in blank:
             return answer
     return None
+
 
 # A player is prompted to replace words in
 # statement, which appear in easy_string, medium_string or hard_string with
 # their own words.
-
-
 def play_game(guess_input, statement, answers):
     replaced = []
     guesses = 0
     statement = statement.split()
     while guesses < guess_input:
-        for user_answer in statement:
-            replacement = answer_in_statement(user_answer, answers)
+        for blank in statement:
+            replacement = answer_in_statement(blank, answers)
             if replacement is not None:
-                user_input = raw_input("Type in a: " + replacement + " ")
-                word = user_answer.replace(replacement, user_input)
-                replaced.append(word)
+                user_input = raw_input("What should be submitted for" +
+                                       replacement + "? ")
+                blank = blank.replace(replacement, user_input)
+                replaced.append(blank)
             else:
-                replaced.append(word)
+                replaced.append(blank)
         replaced = " ".join(replaced)
     return replaced
+
+print Difficulty()[0]  # returns just the first function value
