f7e025fdd7fd6ee82310456aa22facf15a341070
Gustavo Valverde <g.valverde02@gmail.com>
2016-03-29 00:56:17 +0000

    Change code for the final solutions. Lots of changes, but WORKING!

--- Python/Stage 2/fill-in-the-blanks.py
+++ Python/Stage 2/fill-in-the-blanks.py
@@ -13,27 +13,37 @@ dictionary, tuple, and ___4___ or can be more complicated such as objects and
 lambda functions.'''
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def game_level():
     """This function selects the difficulty chosen by user, and asks for the
     number of guesses the user wants for that difficulty
     """
     print "Please select a game difficulty by typing it in!"
     print "Possible choices include easy, medium, and hard."
-    level_input = raw_input("What's your desired difficulty? ").lower()
+    level_input = raw_input("What's your desired difficulty? " + "\n").lower()
     if level_input == "easy":
-        print "You've chosen easy!"
+        print "You've chosen easy!" + "\n"
         answers = easy_answers
         statement = easy_statement
     elif level_input == "medium":
-        print "You've chosen medium!"
+        print "You've chosen medium!" + "\n"
         answers = medium_answers
         statement = medium_statement
     elif level_input == "hard":
-        print "You've chosen hard!"
+        print "You've chosen hard!" + "\n"
         answers = hard_answers
         statement = hard_statement
     else:
-        print "That's not an option!"
+        print "That's not an option!" + "\n"
     return (statement, answers)
 
 statement, answers = game_level()
@@ -46,34 +56,35 @@ def answer_check(answers, answer_index, user_answer):
 
 
 def play_game(statement, answers, blanks):
-    print "\n" + statement
+    print "Now you must fill the blanks here:" + "\n" + statement
     print "How many guesses would you like per problem?"
     attempts_input = raw_input("Please enter a positive integer number: ")
     attempts = 0
-    statement = statement.split()
     answer_index = 0
-    for blank in enumerate(statement):
-        if attempts < int(attempts_input):
-            if blank in blanks:
-                user_answer = raw_input("\n" + "What should be submitted for" +
-                                        blank + "? " + "\n")
-                good_answer = answer_check(answers, answer_index, user_answer)
-                if good_answer is not None:
-#                    statement[s_index] = good_answer
-                    statement = " ".join(statement)
-                    statement = statement.replace(blank, good_answer)
-                    print statement + "\n"
-                    answer_index += 1
+    for blank in blanks:
+        if answer_index < len(answers):
+            correct = False
+            while not correct:
+                if attempts < int(attempts_input):
+                    user_answer = raw_input("\n" + "What should be submitted for" + blank + "? " + "\n")
+                    good_answer = answer_check(answers, answer_index, user_answer)
+                    if good_answer is not None:
+                        statement = statement.replace(blank, good_answer)
+                        print statement + "\n"
+                        answer_index += 1
+                        correct = True
+                    else:
+                        attempts += 1
+                        if attempts == int(attempts_input):
+                            break
+                        print "\n" + "That's the wrong answer, please try again"
+                        print "You have " + str(int(attempts_input) - attempts)
+                        + " attempt(s) left" + "\n"
+                        print statement + "\n"
                 else:
-                    print "\n" + "That's the wrong answer, please try again"
-                    attempts += 1
-                    print "You have " + str(int(attempts_input) - attempts) + " attempt(s) left" + "\n"
-                    replaced = " ".join(statement)
-                    print replaced + "\n"
-        else:
-            print "Sorry, you ran out of attempts"
-            break
+                    print "Sorry, you ran out of attempts"
+                    break
     if attempts < int(attempts_input):
-        print "*******This is your final statement******* \n" + replaced
+        print "       ******* Great, you completed the Quizz *******" + "\n"
 
 play_game(statement, answers, blanks)
