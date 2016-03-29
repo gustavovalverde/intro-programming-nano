ab42d39f21a1a5f13ea567378686df948c7c2d08
Gustavo Valverde <g.valverde02@gmail.com>
2016-03-29 02:10:58 +0000

    Add colors, medium and hard options to test

--- Python/Stage 2/fill-in-the-blanks.py
+++ Python/Stage 2/fill-in-the-blanks.py
@@ -12,6 +12,24 @@ to return. ___2___ can be standard data types such as string, number,
 dictionary, tuple, and ___4___ or can be more complicated such as objects and
 lambda functions.'''
 
+medium_answers = ["function", "arguments", "None", "set"]
+
+medium_statement = '''A ___1___ is created with the def keyword. You specify the
+inputs a ___1___ takes by adding ___2___ separated by commas between the
+parentheses. ___1___s by default return ___3___ if you don't specify the value
+to return. ___2___ can be standard data types such as string, number,
+dictionary, tuple, and ___4___ or can be more complicated such as objects and
+lambda functions.'''
+
+hard_answers = ["function", "arguments", "None", "set"]
+
+hard_statement = '''A ___1___ is created with the def keyword. You specify the
+inputs a ___1___ takes by adding ___2___ separated by commas between the
+parentheses. ___1___s by default return ___3___ if you don't specify the value
+to return. ___2___ can be standard data types such as string, number,
+dictionary, tuple, and ___4___ or can be more complicated such as objects and
+lambda functions.'''
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -29,21 +47,21 @@ def game_level():
     """
     print "Please select a game difficulty by typing it in!"
     print "Possible choices include easy, medium, and hard."
-    level_input = raw_input("What's your desired difficulty? " + "\n").lower()
+    level_input = raw_input(bcolors.OKBLUE + "What's your desired difficulty? " + bcolors.ENDC).lower()
     if level_input == "easy":
-        print "You've chosen easy!" + "\n"
+        print bcolors.OKGREEN + "You've chosen easy!" + bcolors.ENDC + "\n"
         answers = easy_answers
         statement = easy_statement
     elif level_input == "medium":
-        print "You've chosen medium!" + "\n"
+        print bcolors.OKGREEN + "You've chosen medium!" + bcolors.ENDC + "\n"
         answers = medium_answers
         statement = medium_statement
     elif level_input == "hard":
-        print "You've chosen hard!" + "\n"
+        print bcolors.OKGREEN + "You've chosen hard!" + bcolors.ENDC + "\n"
         answers = hard_answers
         statement = hard_statement
     else:
-        print "That's not an option!" + "\n"
+        print bcolors.FAIL + "That's not an option!" + bcolors.ENDC + "\n"
     return (statement, answers)
 
 statement, answers = game_level()
@@ -56,9 +74,9 @@ def answer_check(answers, answer_index, user_answer):
 
 
 def play_game(statement, answers, blanks):
-    print "Now you must fill the blanks here:" + "\n" + statement
-    print "How many guesses would you like per problem?"
-    attempts_input = raw_input("Please enter a positive integer number: ")
+    print bcolors.OKBLUE + "Now you must fill the blanks here:" + "\n" + bcolors.ENDC + statement + "\n"
+    print bcolors.OKBLUE + "How many guesses would you like per problem?" + bcolors.ENDC
+    attempts_input = raw_input(bcolors.OKBLUE + "Please enter a positive integer number: " + bcolors.ENDC)
     attempts = 0
     answer_index = 0
     for blank in blanks:
@@ -66,10 +84,11 @@ def play_game(statement, answers, blanks):
             correct = False
             while not correct:
                 if attempts < int(attempts_input):
-                    user_answer = raw_input("\n" + "What should be submitted for" + blank + "? " + "\n")
+                    user_answer = raw_input(bcolors.OKBLUE + "\n" + "What should be submitted for" + blank + "? " + bcolors.ENDC + "\n")
                     good_answer = answer_check(answers, answer_index, user_answer)
                     if good_answer is not None:
                         statement = statement.replace(blank, good_answer)
+                        print bcolors.OKGREEN + "Good, Thats the answer" + bcolors.ENDC + "\n"
                         print statement + "\n"
                         answer_index += 1
                         correct = True
@@ -77,14 +96,13 @@ def play_game(statement, answers, blanks):
                         attempts += 1
                         if attempts == int(attempts_input):
                             break
-                        print "\n" + "That's the wrong answer, please try again"
-                        print "You have " + str(int(attempts_input) - attempts)
-                        + " attempt(s) left" + "\n"
+                        print "\n" + bcolors.FAIL + "That's the wrong answer, please try again" + bcolors.ENDC
+                        print bcolors.WARNING + "You have " + str(int(attempts_input) - attempts) + " attempt(s) left" + bcolors.ENDC + "\n"
                         print statement + "\n"
                 else:
-                    print "Sorry, you ran out of attempts"
+                    print bcolors.FAIL + "Sorry, you ran out of attempts" + bcolors.ENDC
                     break
     if attempts < int(attempts_input):
-        print "       ******* Great, you completed the Quizz *******" + "\n"
+        print bcolors.GREEN + "       ******* Great, you completed the Quizz *******" + "\n" + bcolors.ENDC
 
 play_game(statement, answers, blanks)
