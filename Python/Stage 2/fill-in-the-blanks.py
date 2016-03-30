8ff50357d084f62deed5cf5adc8fe6427dcc87ad
Gustavo Valverde <g.valverde02@gmail.com>
2016-03-30 19:24:29 +0000

    Add recursion, some "error handling" and better comments

--- Python/Stage 2/fill-in-the-blanks.py
+++ Python/Stage 2/fill-in-the-blanks.py
@@ -37,35 +37,49 @@ hardAnswers = ["function", "arguments", "None", "set"]
 
 hardBlanks = ["___1___", "___2___", "___3___", "___4___"]
 
-quizzes = [easyQuiz, mediumQuiz, hardQuiz]
-answers = [easyAnswers, mediumAnswers, hardAnswers]
-blanks = [easyBlanks, mediumBlanks, hardBlanks]
+quizzes = [easyQuiz, mediumQuiz, hardQuiz]  # list for quizzes strings
+answers = [easyAnswers, mediumAnswers, hardAnswers]  # list for answers lists
+blanks = [easyBlanks, mediumBlanks, hardBlanks]  # list for blanks lists
+
+# lists for each level with its quiz, answers and blanks
 easy = [quizzes[0], answers[0], blanks[0]]
 medium = [quizzes[1], answers[1], blanks[1]]
 hard = [quizzes[2], answers[2], blanks[2]]
+
+# Dictionary refering a string to a variable for later use in game_level().
+# This is for user input validation.
 levels = {'easy': easy, 'medium': medium, 'hard': hard}
 
 
 def game_level():
-    """This function selects the difficulty chosen by user, and asks for the
-    number of guesses the user wants for that difficulty
+    """This function selects the difficulty chosen by user, compares it with
+    the 'levels' dictionary and returns all the variables needed for a level.
+    It also asks the user for the number of guesses the user wants for the
+    chosen diffilcuty, for later use in play_game.
     """
     print "Please select a game difficulty by typing it in!"
     print "Possible choices include easy, medium, and hard."
     choice = raw_input("What's your desired difficulty? ").lower()
-    if choice in levels:
-        choosedLevel = levels[choice]
-    else:
-        print "That's not an option!" + "\n"
+    if choice not in levels:
+        print "\n" + "That's not an option!" + "\n"
+        game_level()
+    choosedLevel = levels[choice]
 
     print "\n" + "Quiz for this difficulty:" + "\n" + choosedLevel[0] + "\n"
     "\n" + "How many guesses would you like per problem?"
     attempts_input = raw_input("Please enter a positive integer number: ")
-
+    while isinstance(attempts_input, int) is False:
+        print "\n" + "Sorry, the program needs an integer" + "\n"
+        attempts_input = int(raw_input("Please enter a positive integer number: "))
     return choosedLevel, attempts_input
 
 
 def play_game(choosedLevel):
+    """This receives a tuple with choosedLevel and attempts_input in it.
+    It takes the first value to iterate over and validate each 'answer' with
+    the respective 'blank' using a for loop and a zip. If the user answer is
+    correct it keeps asking for the next 'blank', if its not then it loops in
+    a 'while' until the right answer its given if attempts still remains"""
     quiz_string = choosedLevel[0][0]
     # Answer_index 0 -->  choosedLevel[0][1][0]
     # Blanks list -->  choosedLevel[0][2]
@@ -75,16 +89,16 @@ def play_game(choosedLevel):
                                     blank + "? " + "\n")
             while user_answer != answer and attempts <= int(choosedLevel[1]):
                 attempts += 1
-                if attempts == int(choosedLevel[1]):
+                if attempts == choosedLevel[1]:
                     return "\n" + "Sorry, you ran out of attempts" + "\n"
-                print "\n" + "That's not the answer, try again" + "\n" + "You have " + str(int(choosedLevel[1]) - attempts) + " attempt(s)"
+                print "\n" + "That's not the answer, try again" + "\n" + "You have " + str(choosedLevel[1] - attempts) + " attempt(s)"
                 "left" + "\n" + quiz_string + "\n"
                 user_answer = raw_input("\n" + "What should be submitted for" +
                                         blank + "? " + "\n")
 
             quiz_string = quiz_string.replace(blank, user_answer)
             print "\n" + "Good, thats correct" + "\n \n" + quiz_string + "\n"
-    if attempts < int(choosedLevel[1]):
+    if attempts < choosedLevel[1]:
         return "       ******* Great, you completed the Quizz *******" + "\n"
 
 print play_game(game_level())
