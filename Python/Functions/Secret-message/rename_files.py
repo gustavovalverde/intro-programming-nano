1f8eb1c63fb808e50e6c2b5873af6d6d544a52c9
Gustavo Valverde <g.valverde02@gmail.com>
2016-04-17 13:26:07 +0000

    Add last changes for final working commit

--- Python/Functions/Secret-message/rename_files.py
+++ Python/Functions/Secret-message/rename_files.py
@@ -2,13 +2,11 @@ import os
 
 
 def rename_files():
-    files_path = os.listdir("C:\Users\GustavoA\intro-programming-nano"
-                            "\Python\Functions\Secret-message\prank")
-    saved_path = os.getcwd()
-    os.chdir("C:\Users\GustavoA\intro-programming-nano\Python\Functions"
-             "\Secret-message\prank")
+    files_path = os.listdir("/home/vagrant/intro-programming"
+                            "/Python/Functions/Secret-message/prank")
     for file_name in files_path:
         os.rename(file_name, file_name.translate(None, "0123456789"))
-    os.chdir(saved_path)
 
+print os.getcwd()
 rename_files()
+
